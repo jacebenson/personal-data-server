e08d929a0a27a21f80e774c0e243baafc84579ae
Jace Benson <638764+jacebenson@users.noreply.github.com>
2025-08-20 22:00:22 -0500

    okay

--- app/controllers/personal_controller.rb
+++ app/controllers/personal_controller.rb
@@ -10,5 +10,12 @@ class PersonalController < ApplicationController
     @contacts_count = current_user.contacts.count
     @calendar_events_count = current_user.calendar_events.count
     @content_items_count = 0
+    
+    # LinkedIn connections stats
+    @linkedin_contacts = current_user.contacts.where(source: 'linkedin')
+    @linkedin_contacts_count = @linkedin_contacts.count
+    @linkedin_contacts_with_companies = @linkedin_contacts.where.not(organization: [nil, '']).count
+    @linkedin_contacts_with_job_titles = @linkedin_contacts.where.not(job_title: [nil, '']).count
+    @linkedin_last_import = @linkedin_contacts.maximum(:imported_at)
   end
 end
